985d37177ab334f72815e4ffd8623964013bad64
Jacob Dueholm <jacob@stacktrace.dk>
2014-09-02 23:22:15 +0200

    I really need to refactor :)

--- lb.el
+++ lb.el
@@ -1,6 +1,8 @@
 (require 's)
 
-
+(defvar jd/lb-info-name-list nil
+"This list holds the implementation name and the friendly name of Plex objects"
+)
 
 (defun jd/lb-buffer-is-col-xml ()
   "Determines wether the current buffer is visiting af file
@@ -10,15 +12,13 @@ which filename ends with .col.xml"
 
 
 
-
-  ;;  <col seq="0" label="" info="Text" id=" " name="/(FUCKLB5ueA)" class="" width="" hId=" " hName=" " hDoc=" ">1</col>
+  ;;  <col seq="0" label="" info="Text" id=" " name="/(LB5ueA)" class="" width="" hId=" " hName=" " hDoc=" ">1</col>
 (defun jd/lb-build-name-alist ()
   (interactive)
   (let ((_line nil)
 	(col-start 0)
 	(col-end 0)	
 	(wrk-list ()))
-
   (save-excursion 
     (goto-char (point-min))
     (while (and col-start col-end) 
@@ -27,21 +27,21 @@ which filename ends with .col.xml"
       (if (and col-start col-end)
 	  (progn 
 	      (setq _line (buffer-substring-no-properties col-start col-end))
-	      (setq wrk-list (append wrk-list (list _line)))
-
-	      ;;(message (last wrk-list 5))
-	      ))
-
-      	      
-      ))
+	      (setq wrk-list (append wrk-list (list _line)))))))
     (set-buffer (get-buffer-create "*DuggiDebug*")) 
     (erase-buffer)
     (nxml-mode)
-;;    (insert (car wrk-list))
-      (mapcar (lambda (x) 
-		(insert x)		
-		(newline)) wrk-list)))
-
+    (mapcar (lambda (x)
+	      (let 
+		  ((name nil)
+		   (info nil))
+		(string-match "info=\"[[:alpha:] \| [:digit:] \| \s- \| _]*" x)
+		(setq info (match-string 0 x))
+		(string-match " name=\"[[:alpha:] \| [:digit:] \| // \| ( \| )]*" x)
+		(setq name (match-string 0 x))
+		(if (and name info)
+		    (setq jd/lb-info-name-list (append jd/lb-info-name-list (list name info))))))
+	    wrk-list)))
 
 
 
@@ -52,8 +52,6 @@ which filename ends with .col.xml"
   )
 
 (key-chord-define-global "vb" 'jd/lb-test)
-
-
 (provide 'lb)
 ;;; lb.el ends here
 
